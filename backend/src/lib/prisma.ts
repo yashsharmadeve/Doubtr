bc34e188837fb9f81b63603e4b1489472e2d001d
Yash Sharma <yashsharmasbm@gmail.com>
2026-07-12 23:21:11 +0530

    new

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -1,15 +1,4 @@
-// import "dotenv/config";
-// import { PrismaPg } from "@prisma/adapter-pg";
-// import { PrismaClient } from "../../generated/prisma/client";
-
-// const connectionString = `${process.env.DATABASE_URL}`;
-
-// const adapter = new PrismaPg({ connectionString });
-// const prisma = new PrismaClient({ adapter });
-
-// export { prisma };
-
-import { PrismaClient } from "../../generated/prisma/client";
+import { PrismaClient } from "../generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { config } from "../config";
 
